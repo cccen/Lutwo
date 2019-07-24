dbc3a7fb9e90bff8ba0f3606d86d576511965b12
cccen <cccen@163.com>
2019-07-24 15:06:02 +0800

    命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,18 +5,10 @@
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-<div class="navbar navbar-dark navbar-expand-lg bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="{{Route('home')}}">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="{{Route('help')}}">帮助</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{Route('about')}}">关于</a></li>
-        </ul>
-    </div>
-
-</div>
+@include('layouts._header')
 <div class="container">
     @yield('content')
 </div>
+@include('layouts._footer')
 </body>
 </html>
