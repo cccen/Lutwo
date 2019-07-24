7fedef8269a73beabe09088a3a3c5e37aae05511
cccen <cccen@163.com>
2019-07-24 14:49:16 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,14 +2,15 @@
 <html>
 <head>
     <title>@yield('title','Second App')</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-<div class="navbar-dark navbar-nav">
+<div class="navbar navbar-dark navbar-expand-lg bg-dark">
     <div class="container">
         <a class="navbar-brand" href="{{Route('home')}}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a href="{{Route('help')}}">帮助</a></li>
-            <li class="nav-item"><a href="{{Route('about')}}">关于</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{Route('help')}}">帮助</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{Route('about')}}">关于</a></li>
         </ul>
     </div>
 
