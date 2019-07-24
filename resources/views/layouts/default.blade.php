058c6055bfd0f1a976ee1b59b92932fa6f966892
cccen <cccen@163.com>
2019-07-24 09:10:33 +0800

    two

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,18 @@
     <title>@yield('title','Second App')</title>
 </head>
 <body>
+<div class="navbar-dark navbar-nav">
+    <div class="container">
+        <a class="navbar-brand" href="{{Route('home')}}">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="{{Route('help')}}">帮助</a></li>
+            <li class="nav-item"><a href="{{Route('about')}}">关于</a></li>
+        </ul>
+    </div>
+
+</div>
+<div class="container">
     @yield('content')
+</div>
 </body>
 </html>
