349ce6ff2ff2f25cd88717a1939c8d1f3e7918d3
cccen <cccen@163.com>
2019-07-26 09:29:11 +0800

    创建会话

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand" href="{{Route('home')}}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
             <li class="nav-item"><a class="nav-link" href="{{Route('help')}}">帮助</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{Route('about')}}">关于</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{Route('login')}}">登录</a></li>
         </ul>
     </div>
 
